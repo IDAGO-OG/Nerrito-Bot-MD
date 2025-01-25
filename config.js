fc6e3568f437039a322ed9871ea982ddebe4e2a7
DIEGO-OG <do904498@gmail.com>
2025-01-25 18:58:26 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import moment from 'moment-timezone';
 
 global.botnumber = "";
 global.confirmCode = "";
-global.authFile = `NerritoSession`; // Cambiado a NerritoSession
+global.authFile = `NerritoSession/nerrito-bot-md.txt`; // Cambiado a NerritoSession
 
 // Cambiar a true si el Bot responde a sus comandos con otros comandos.
 // Cambiar a false para usar el Bot desde el mismo numero del Bot.
